595103d6d4217bf583f3f771d21f3bbcbd996080
Weiguang <chen.weiguang@gmail.com>
2019-07-04 21:39:53 +0800

    Initial implementation (#1)
    * Create a skeletal structure to get the k8s docker image built
    
    * Add full fledge implementation
    
    * Fix the build with patched JAR for Hadoop 3
    
    * Allow sudo for building
    
    * Fix travis deps
    
    * Start with jdk8 for travis
    
    * Switch to oraclejdk8
    
    * Skip log lines to make it pass

---
+++ make-distribution.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -euox pipefail
+
+DOCKER_REPO=${DOCKER_REPO:-guangie88/spark-k8s}
+HIVE_HADOOP3_HIVE_EXEC_URL=${HIVE_HADOOP3_HIVE_EXEC_URL:-https://github.com/guangie88/hive-exec-jar/releases/download/1.2.1.spark2-hadoop3/hive-exec-1.2.1.spark2.jar}
+
+if ! which git >/dev/null; then
+    >&2 echo "Cannot find git in PATH!"
+    exit 1
+fi
+
+# Get the Spark source files set-up ready
+if [ ! -d "spark" ] ; then
+    git clone https://github.com/apache/spark.git -b v${SPARK_VERSION}
+else
+    pushd spark >/dev/null
+    git reset --hard
+    git checkout v${SPARK_VERSION}
+    popd >/dev/null
+fi
+
+HIVE_INSTALL_FLAG=$(if [ "${WITH_HIVE}" = "true" ]; then echo "-Phive"; fi)
+PYSPARK_INSTALL_FLAG=$(if [ "${WITH_PYSPARK}" = "true" ]; then echo "--pip"; fi)
+
+pushd spark >/dev/null
+
+# The build is very verbose and exceeds max log length, need to reduce using awk
+./dev/make-distribution.sh \
+    ${PYSPARK_INSTALL_FLAG} --name spark-${SPARK_VERSION}_hadoop-${HADOOP_VERSION} \
+    -Phadoop-$(echo ${HADOOP_VERSION} | cut -c 1-3) \
+    -Dhadoop.version=${HADOOP_VERSION} \
+    -Pkubernetes \
+    ${HIVE_INSTALL_FLAG} \
+    -DskipTests | awk 'NR % 50 == 0'
+
+# Replace Hive for Hadoop 3 since Hive 1.2.1 does not officially support Hadoop 3
+# Note docker-image-tool.sh takes the jars from assembly/target/scala-2.11/jars
+if [ "${WITH_HIVE}" = "true" ] && [ "$(echo ${HADOOP_VERSION} | cut -c 1)" = "3" ]; then
+    (cd assembly/target/scala-2.11/jars && curl -LO ${HIVE_HADOOP3_HIVE_EXEC_URL})
+    (cp assembly/target/scala-2.11/jars/hive-exec-1.2.1.spark2.jar dist/jars/)
+fi
+
+# There is no way to rename the Docker image, so we simply retag
+./bin/docker-image-tool.sh -r ${DOCKER_REPO} -t ${SPARK_VERSION}_hadoop-${HADOOP_VERSION} build
+
+docker tag "${DOCKER_REPO}/spark:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}" "${DOCKER_REPO}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+docker tag "${DOCKER_REPO}/spark-r:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}" "${DOCKER_REPO}-r:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+docker tag "${DOCKER_REPO}/spark-py:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}" "${DOCKER_REPO}-py:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+
+popd >/dev/null
