2900ae8059d479071880ad097a4ab40e8a198a18
Chen Weiguang <chen.weiguang@gmail.com>
2019-07-08 23:43:11 +0800

    Shellcheck and fix every lint

--- make-distribution.sh
+++ make-distribution.sh
@@ -32,19 +32,19 @@ pushd spark >/dev/null
 
 # The build is very verbose and exceeds max log length, need to reduce using awk
 ./dev/make-distribution.sh \
-    ${PYSPARK_INSTALL_FLAG} --name spark-${SPARK_VERSION}_hadoop-${HADOOP_VERSION} \
-    -Phadoop-$(echo ${HADOOP_VERSION} | cut -c 1-3) \
-    -Dhadoop.version=${HADOOP_VERSION} \
+    "${PYSPARK_INSTALL_FLAG}" --name "spark-${SPARK_VERSION}_hadoop-${HADOOP_VERSION}" \
+    "-Phadoop-$(echo "${HADOOP_VERSION}" | cut -c 1-3)" \
+    "-Dhadoop.version=${HADOOP_VERSION}" \
     -Pkubernetes \
-    ${HIVE_INSTALL_FLAG} \
+    "${HIVE_INSTALL_FLAG}" \
     -DskipTests | awk 'NR % 10 == 0'
 
 # Replace Hive for Hadoop 3 since Hive 1.2.1 does not officially support Hadoop 3
 # Note docker-image-tool.sh takes the jars from assembly/target/scala-2.*/jars
-if [ "${WITH_HIVE}" = "true" ] && [ "$(echo ${HADOOP_VERSION} | cut -c 1)" = "3" ]; then
+if [ "${WITH_HIVE}" = "true" ] && [ "$(echo "${HADOOP_VERSION}" | cut -c 1)" = "3" ]; then
     JARS_DIR="$(find assembly -type d -name 'scala-2.*')"
-    (cd ${JARS_DIR} && curl -LO ${HIVE_HADOOP3_HIVE_EXEC_URL})
-    (cp ${JARS_DIR}/hive-exec-1.2.1.spark2.jar dist/jars/)
+    (cd "${JARS_DIR}" && curl -LO "${HIVE_HADOOP3_HIVE_EXEC_URL}")
+    (cp "${JARS_DIR}/hive-exec-1.2.1.spark2.jar" dist/jars/)
 fi
 
 GIT_REV="$(git rev-parse HEAD | cut -c 1-7)"
@@ -55,11 +55,11 @@ else
 fi
 
 # There is no way to rename the Docker image, so we simply retag
-./bin/docker-image-tool.sh -r ${DOCKER_REPO} -t ${SPARK_LABEL}_hadoop-${HADOOP_VERSION} build
+./bin/docker-image-tool.sh -r "${DOCKER_REPO}" -t "${SPARK_LABEL}_hadoop-${HADOOP_VERSION}" build
 
 docker tag "${DOCKER_REPO}/spark:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}" "${DOCKER_REPO}:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
 
-SPARK_XY_VERSION="$(echo ${SPARK_VERSION} | cut -c 1-3)"
+SPARK_XY_VERSION="$(echo "${SPARK_VERSION}" | cut -c 1-3)"
 if [ "${SPARK_XY_VERSION}" != "2.3" ]; then  # >= 2.4 or master
     docker tag "${DOCKER_REPO}/spark-r:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}" "${DOCKER_REPO}-r:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
     docker tag "${DOCKER_REPO}/spark-py:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}" "${DOCKER_REPO}-py:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
--- push-images.sh
+++ push-images.sh
@@ -15,7 +15,7 @@ fi
 
 docker push "${DOCKER_REPO}:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
 
-SPARK_XY_VERSION="$(echo ${SPARK_VERSION} | cut -c 1-3)"
+SPARK_XY_VERSION="$(echo "${SPARK_VERSION}" | cut -c 1-3)"
 if [ "${SPARK_XY_VERSION}" != "2.3" ]; then  # >= 2.4 or master
     docker push "${DOCKER_REPO}-r:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
     docker push "${DOCKER_REPO}-py:${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
