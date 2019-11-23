99d5578123b936184926eb33e5a283bc5a540768
Weiguang <chen.weiguang@gmail.com>
2019-11-23 23:46:34 +0800

    ci: add github actions for CI (#11)
    * ci: add github actions for CI
    
    * Add 3.0.0 RC versions for build
    
    * Fix YML config
    
    * Add secrets for push
    
    * Change event_name to single quote
    
    * Fix for 3.0.0 Spark version
    
    * Switch to bash double square brackets
    
    * Remove 3.0.0 and add dist version
    
    * Remove travis for CI

--- make-distribution.sh
+++ make-distribution.sh
@@ -8,14 +8,10 @@ if ! command -v git >/dev/null; then
     exit 1
 fi
 
-if [ "${SPARK_VERSION}" = "master" ]; then
-    SPARK_VERSION_TAG="master"
-else
-    SPARK_VERSION_TAG="v${SPARK_VERSION}"
-fi
+SPARK_VERSION_TAG="v${SPARK_VERSION}"
 
 # Get the Spark source files set-up ready
-if [ ! -d "spark" ]; then
+if [[ ! -d "spark" ]]; then
     git clone https://github.com/apache/spark.git -b "${SPARK_VERSION_TAG}"
 else
     pushd spark >/dev/null
@@ -25,12 +21,12 @@ else
     popd >/dev/null
 fi
 
-[ "${WITH_HIVE}" = "true" ] && HIVE_INSTALL_FLAG="yes"
-[ "${WITH_PYSPARK}" = "true" ] && PYSPARK_INSTALL_FLAG="yes"
+[[ "${WITH_HIVE}" == "true" ]] && HIVE_INSTALL_FLAG="yes"
+[[ "${WITH_PYSPARK}" == "true" ]] && PYSPARK_INSTALL_FLAG="yes"
 
 pushd spark >/dev/null
 
-[ "${SPARK_VERSION}" != "master" ] && HADOOP_OVERRIDE_FLAG="yes"
+HADOOP_OVERRIDE_FLAG="yes"
 
 # The build is very verbose and exceeds max log length, need to reduce using awk
 # TERM issue: https://github.com/lihaoyi/mill/issues/139#issuecomment-366818171
@@ -44,7 +40,7 @@ TERM=xterm-color ./dev/make-distribution.sh \
 
 # Replace Hive for Hadoop 3 since Hive 1.2.1 does not officially support Hadoop 3 when using Spark 2.y.z
 # Note docker-image-tool.sh takes the jars from assembly/target/scala-2.*/jars
-if [ "${SPARK_VERSION}" != "master" ] && [ "${WITH_HIVE}" = "true" ] && [ "$(echo "${HADOOP_VERSION}" | cut -c 1)" -eq 3 ]; then
+if [[ "${WITH_HIVE}" = "true" ]] && [[ "$(echo "${HADOOP_VERSION}" | cut -c 1)" -eq 3 ]]; then
     HIVE_EXEC_JAR_NAME="hive-exec-1.2.1.spark2.jar"
     TARGET_JAR_PATH="$(find assembly -type f -name "${HIVE_EXEC_JAR_NAME}")"
     curl -LO "${HIVE_HADOOP3_HIVE_EXEC_URL}" && mv "${HIVE_EXEC_JAR_NAME}" "${TARGET_JAR_PATH}"
@@ -53,30 +49,16 @@ if [ "${SPARK_VERSION}" != "master" ] && [ "${WITH_HIVE}" = "true" ] && [ "$(ech
 fi
 
 GIT_REV="$(git rev-parse HEAD | cut -c 1-7)"
-if [ "${SPARK_VERSION}" = "master" ]; then
-    SPARK_LABEL="master-${GIT_REV}"
-else
-    SPARK_LABEL="${SPARK_VERSION}"
-fi
+SPARK_LABEL="${SPARK_VERSION}"
 
 TAG_NAME="${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
 
-if [[ "${SPARK_VERSION}" == "master" ]]; then
-    # master branch requires manual specification of the paths of -py and -r Dockerfiles
-    ./bin/docker-image-tool.sh \
-        -r "${IMAGE_NAME}" \
-        -t "${TAG_NAME}" \
-        -p dist/kubernetes/dockerfiles/spark/bindings/python/Dockerfile \
-        -R dist/kubernetes/dockerfiles/spark/bindings/R/Dockerfile \
-        build
-else
-    # branch-2.3 will does not have -py and -r builds
-    # branch-2.4 will auto-infer the -py and -r Dockerfile paths correctly
-    ./bin/docker-image-tool.sh \
-        -r "${IMAGE_NAME}" \
-        -t "${TAG_NAME}" \
-        build
-fi
+# branch-2.3 will does not have -py and -r builds
+# branch-2.4 will auto-infer the -py and -r Dockerfile paths correctly
+./bin/docker-image-tool.sh \
+    -r "${IMAGE_NAME}" \
+    -t "${TAG_NAME}" \
+    build
 
 docker tag "${IMAGE_NAME}/spark:${TAG_NAME}" "${IMAGE_NAME}:${TAG_NAME}"
 
@@ -85,16 +67,16 @@ SPARK_MINOR_VERSION="$(echo "${SPARK_VERSION}" | cut -d '.' -f2)"
 
 # There is no way to rename the Docker image, so we simply retag
 # Spark <= 2.3 does not do -py and -r set-up, therefore we need this check here
-if [[ "${SPARK_VERSION}" == "master" ]] || [[ ${SPARK_MAJOR_VERSION} -ge 3 ]] || [[ ${SPARK_MAJOR_VERSION} -eq 2 && ${SPARK_MINOR_VERSION} -ge 4 ]]; then  # >= 2.4
+if [[ ${SPARK_MAJOR_VERSION} -eq 2 && ${SPARK_MINOR_VERSION} -ge 4 ]]; then  # >= 2.4
     docker tag "${IMAGE_NAME}/spark-r:${TAG_NAME}" "${IMAGE_NAME}-r:${TAG_NAME}"
     docker tag "${IMAGE_NAME}/spark-py:${TAG_NAME}" "${IMAGE_NAME}-py:${TAG_NAME}"
 fi
 
 popd >/dev/null
 
-# Spark 2.4 builds are silly and don't include spark/python/pyspark contents
+# Spark >= 2.4 builds are silly and don't include spark/python/pyspark contents
 # So manually include them
-if [[ "${SPARK_VERSION}" != "master" ]] && [[ ${SPARK_MAJOR_VERSION} -eq 2 && ${SPARK_MINOR_VERSION} -ge 4 ]]; then  # >= 2.4
+if [[ ${SPARK_MAJOR_VERSION} -eq 2 && ${SPARK_MINOR_VERSION} -ge 4 ]]; then  # >= 2.4
     docker build . -f Dockerfile-py -t "${IMAGE_NAME}-py:${TAG_NAME}" \
         --build-arg "IMAGE_NAME=${IMAGE_NAME}" \
         --build-arg "TAG_NAME=${TAG_NAME}"
