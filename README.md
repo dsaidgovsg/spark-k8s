0006ebb7ac9534a0bfe5d97702fbe7dcc720c4ca
Chen Weiguang <chen.weiguang@gmail.com>
2020-01-12 19:51:39 +0800

    chore: change in self tagged naming
    Remove `spark` from the full tag labelling.
    
    Change `DIST_VERSION` to `SELF_VERSION`.

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 ![CI Status](https://img.shields.io/github/workflow/status/guangie88/spark-k8s/CI/master?color=green&label=CI&logo=github&logoColor=white&style=for-the-badge)
 
-Travis setup to generate Spark Docker images meant for running in Kubernetes
-set-up.
+CI setup to generate Spark Docker images meant for running in Kubernetes sset-up.
 
 All the built images here are inherently based on `openjdk8` and Alpine as the
 official build script builds the Docker image based on that.
--- push-images.sh
+++ push-images.sh
@@ -12,13 +12,13 @@ else
     SPARK_LABEL="${SPARK_VERSION}"
 fi
 
-TAG_NAME="${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
-DIST_TAG_NAME="${DIST_VERSION}_spark-${TAG_NAME}"
+TAG_NAME="${SELF_VERSION}_${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
+ALT_TAG_NAME="${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
 
 docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
-docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${DIST_TAG_NAME}"
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${DIST_TAG_NAME}"
+docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${ALT_TAG_NAME}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${ALT_TAG_NAME}"
 
 SPARK_MAJOR_VERSION="$(echo "${SPARK_VERSION}" | cut -d '.' -f1)"
 SPARK_MINOR_VERSION="$(echo "${SPARK_VERSION}" | cut -d '.' -f2)"
@@ -26,13 +26,13 @@ SPARK_MINOR_VERSION="$(echo "${SPARK_VERSION}" | cut -d '.' -f2)"
 if [[ ${SPARK_MAJOR_VERSION} -ge 2 && ${SPARK_MINOR_VERSION} -ge 4 ]]; then  # >= 2.4
     docker tag "${IMAGE_NAME}-r:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${TAG_NAME}"
     docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${TAG_NAME}"
-    docker tag "${IMAGE_NAME}-r:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${DIST_TAG_NAME}"
-    docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${DIST_TAG_NAME}"
+    docker tag "${IMAGE_NAME}-r:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${ALT_TAG_NAME}"
+    docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${ALT_TAG_NAME}"
 
     docker tag "${IMAGE_NAME}-py:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${TAG_NAME}"
     docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${TAG_NAME}"
-    docker tag "${IMAGE_NAME}-py:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${DIST_TAG_NAME}"
-    docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${DIST_TAG_NAME}"
+    docker tag "${IMAGE_NAME}-py:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${ALT_TAG_NAME}"
+    docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${ALT_TAG_NAME}"
 fi
 
 popd >/dev/null
