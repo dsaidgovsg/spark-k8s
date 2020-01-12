0be7b51cbf5d6d9935b4cf72fc878198fecbefe1
Weiguang <chen.weiguang@gmail.com>
2020-01-13 00:50:58 +0800

    feat: add scala version (#20)
    Also fix push tag name.
    
    This changes the tag by attaching the scala version to it.

--- push-images.sh
+++ push-images.sh
@@ -12,8 +12,8 @@ else
     SPARK_LABEL="${SPARK_VERSION}"
 fi
 
-TAG_NAME="${SELF_VERSION}_${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
-ALT_TAG_NAME="${SPARK_LABEL}_hadoop-${HADOOP_VERSION}"
+TAG_NAME="${SELF_VERSION}_${SPARK_LABEL}_hadoop-${HADOOP_VERSION}_scala-${SCALA_VERSION}"
+ALT_TAG_NAME="${SPARK_LABEL}_hadoop-${HADOOP_VERSION}_scala-${SCALA_VERSION}"
 
 docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
@@ -23,13 +23,13 @@ docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${ALT_TAG_NAME}"
 # Python image push
 docker tag "${IMAGE_NAME}-py:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${TAG_NAME}"
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${TAG_NAME}"
-docker tag "${IMAGE_NAME}-py:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${DIST_TAG_NAME}"
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${DIST_TAG_NAME}"
+docker tag "${IMAGE_NAME}-py:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${ALT_TAG_NAME}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-py:${ALT_TAG_NAME}"
 
 # R image push
 docker tag "${IMAGE_NAME}-r:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${TAG_NAME}"
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${TAG_NAME}"
-docker tag "${IMAGE_NAME}-r:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${DIST_TAG_NAME}"
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${DIST_TAG_NAME}"
+docker tag "${IMAGE_NAME}-r:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${ALT_TAG_NAME}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}-r:${ALT_TAG_NAME}"
 
 popd >/dev/null
