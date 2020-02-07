aba21ca7cbf1f699459b4e0fb11ca91498a2ec81
Weiguang <chen.weiguang@gmail.com>
2020-02-08 01:46:21 +0800

    feat(vars): add spark 2.4.5 for building (#21)
    Spark 2.4.5 is also Debian based, thus it is installed together witih
    Spark 3 RC preview.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,10 +2,13 @@
 
 ## v2
 
-- For Spark 3 preview.
+- For Spark 3 preview, but also support Spark 2.4.5 since the K8s Debian based image change was
+  ported to Spark 2.4.5 onwards.
 - Debian-based image since the earliest preview release tag `v3.0.0-preview-rc1`
   uses `openjdk:8-jdk-slim`.
-  See: <https://github.com/apache/spark/blob/v3.0.0-preview-rc1/resource-managers/kubernetes/docker/src/main/dockerfiles/spark/Dockerfile#L18>
+  See:
+  - <https://github.com/apache/spark/blob/v3.0.0-preview-rc1/resource-managers/kubernetes/docker/src/main/dockerfiles/spark/Dockerfile#L18>
+  - <https://github.com/apache/spark/blob/v2.4.5/resource-managers/kubernetes/docker/src/main/dockerfiles/spark/Dockerfile#L18>
 
 ## v1
 
--- make-distribution.sh
+++ make-distribution.sh
@@ -35,6 +35,20 @@ TERM=xterm-color ./dev/make-distribution.sh \
     ${HIVE_INSTALL_FLAG:+"-Phive"} \
     -DskipTests
 
+SPARK_MAJOR_VERSION="$(echo "${SPARK_VERSION}" | cut -d '.' -f1)"
+HADOOP_MAJOR_VERSION="$(echo "${HADOOP_VERSION}" | cut -d '.' -f1)"
+HIVE_HADOOP3_HIVE_EXEC_URL=${HIVE_HADOOP3_HIVE_EXEC_URL:-https://github.com/guangie88/hive-exec-jar/releases/download/1.2.1.spark2-hadoop3/hive-exec-1.2.1.spark2.jar}
+
+# Replace Hive for Hadoop 3 since Hive 1.2.1 does not officially support Hadoop 3 when using Spark 2.y.z
+# Note docker-image-tool.sh takes the jars from assembly/target/scala-2.*/jars
+if [[ "${WITH_HIVE}" = "true" ]] && [[ "${SPARK_MAJOR_VERSION}" -eq 2 ]] && [[ "${HADOOP_MAJOR_VERSION}" -eq 3 ]]; then
+    HIVE_EXEC_JAR_NAME="hive-exec-1.2.1.spark2.jar"
+    TARGET_JAR_PATH="$(find assembly -type f -name "${HIVE_EXEC_JAR_NAME}")"
+    curl -LO "${HIVE_HADOOP3_HIVE_EXEC_URL}" && mv "${HIVE_EXEC_JAR_NAME}" "${TARGET_JAR_PATH}"
+    # Spark <= 2.4 uses ${TARGET_JAR_PATH} for Docker COPY, but Spark >= 3 uses dist/jars/
+    cp "${TARGET_JAR_PATH}" "dist/jars/"
+fi
+
 GIT_REV="$(git rev-parse HEAD | cut -c 1-7)"
 SPARK_LABEL="${SPARK_VERSION}"
 
